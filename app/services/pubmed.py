bf431aa148d69d18fe2f2142e7cce6737af0494c
Saba Daftari <sabadaftari@gmail.com>
2025-02-24 17:09:20 -0800

    search through europe pmc added

--- app/services/pubmed.py
+++ app/services/pubmed.py
@@ -2,8 +2,9 @@ import xmltodict
 import requests
 from typing import Any
 
-def fetch_pubmed_articles(query: Any, max_results=10):
-    base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+def fetch_pubmed_articles(query: Any, max_results:int =10):
+
+    endpoint = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
     params = {
         "db": "pubmed",
         "term": query,  
@@ -11,7 +12,7 @@ def fetch_pubmed_articles(query: Any, max_results=10):
         "retmax": max_results,
     }
 
-    response = requests.get(base_url, params=params)
+    response = requests.get(endpoint, params=params)
     data = xmltodict.parse(response.content)
     
     article_ids = data["eSearchResult"].get("IdList", {}).get("Id", [])
@@ -44,5 +45,50 @@ def fetch_pubmed_articles(query: Any, max_results=10):
             })
     return articles
 
-fetch_pubmed_articles("coronary artery disease AND drug")
+# fetch_pubmed_articles("coronary artery disease drug")
+
+from datetime import datetime, timedelta
+
+def fetch_europe_pmc_articles(query, max_results=10, past_num_days=30):
+    # Calculate the date range
+    end_date = datetime.today().strftime("%Y-%m-%d")
+    start_date = (datetime.today() - timedelta(days=past_num_days)).strftime("%Y-%m-%d")
+
+    endpoint = "https://www.ebi.ac.uk/europepmc/webservices/rest/search"
+
+    # Query includes the date filter
+    search_query = f"{query} AND FIRST_PDATE:[{start_date} TO {end_date}]"
+    
+    params = {
+        "query": search_query,  # Ensures results are from BioRxiv
+        "format": "json",
+        "pageSize": max_results
+    }
+
+    response = requests.get(endpoint, params=params)
+    
+    # in case we get API error
+    if response.status_code != 200:
+        print(f"Error: Failed to fetch data. Status Code: {response.status_code}")
+        return []
+
+    data = response.json()
+
+    articles = []
+    for item in data.get("resultList", {}).get("result", []):
+        articles.append({
+            "title": item.get("title", "No title available"),
+            "authors": item.get("authorString", "No authors available"),
+            "abstract": item.get("abstractText", "No abstract available"),
+            "doi": item.get("doi", "No DOI available"),
+            "url": f"https://europepmc.org/article/{item.get('source', '')}/{item.get('id', '')}"
+        })
+    
+    if not articles:
+        print(f"We have found no articles pulished on Europe PMC during the past {past_num_days}.")
+
+    return articles
+
+europe_pmc_results = fetch_europe_pmc_articles("coronary artery disease drug")
+
 
