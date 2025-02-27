45106e6f9a0bae765198c57e293826375b171697
Saba Daftari <sabadaftari@gmail.com>
2025-02-27 11:10:00 -0800

    create config file to load .env variables

---
+++ app/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")  # or another model
+EMBEDDING_DIMENSION = 1536
